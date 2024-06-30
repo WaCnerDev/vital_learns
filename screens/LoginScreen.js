925b1044854b47550aa496484128623f285a160f
xWOLF247 <jarolsala@gmail.com>
2024-06-29 22:38:02 -0600

    Ajustes en LoginScreen, RegisterScreen, se agrego la DB y permite registrar usuarios

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,84 +1,72 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground, Image } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import LoginHeader from "../components/LoginHeader";
+import { FontAwesome, Entypo, AntDesign } from "@expo/vector-icons";
 
 export default function LoginScreen() {
     const navigation = useNavigation();
-   
+
     return (
-       
+        <ScrollView>
             <View style={styles.container}>
-                <Image source={require('../img/brainVitalLearns.png')} style={styles.logo} />
-                <Text>VitalLearn</Text>
+                <LoginHeader/>
                 <Text style={styles.txtTitulo}>Welcome To VitalLearns!</Text>
                 <Text style={styles.txtSubtitulo}>Log in with your VitalLearn account</Text>
-              
+
                 <Text style={styles.txtLogin}>Enter your email</Text>
                 <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.txtInput}
-                        placeholderTextColor="#888"
-                    />
+                    <FontAwesome style={styles.icon} name="user-circle-o" size={30} color="#B72424" />
+                    <TextInput style={styles.txtInput} />
                 </View>
+
                 <Text style={styles.txtLogin}>Enter your password</Text>
                 <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.txtInput}
-                        placeholderTextColor="#888"
-                        secureTextEntry={true}
-                    />
+                    <Entypo style={styles.icon} name="lock" size={30} color="#B72424" />
+                    <TextInput style={styles.txtInput} secureTextEntry={true} />
+                    <AntDesign name="eyeo" size={24} color="black" />
                 </View>
+                
                 <TouchableOpacity onPress={() => navigation.navigate("RecoverScreen")}>
                     <Text style={styles.txtForgot}>Forgot your password?</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate("Main")}>
-                    <LinearGradient
-                        colors={['#FF0000', '#FF0000']}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 1 }}
-                        style={styles.btnLogin}
-                    >
-                        <Text style={styles.txtLoginButton}>Sign In</Text>
-                    </LinearGradient>
+
+                <TouchableOpacity onPress={() => navigation.navigate("RecoverScreen")} style={styles.btnLogin}>
+                    <Text style={styles.txtLoginButton}>Sign In</Text>
                 </TouchableOpacity>
+
                 <TouchableOpacity onPress={() => navigation.navigate("RegisterScreen")}>
-                    <Text style={styles.txtLogin}>
+                    <Text style={styles.txtNonAccount}>
                         Don't have an account?
                         <Text style={styles.txtSignUp}> Sign Up Now</Text>
                     </Text>
                 </TouchableOpacity>
                 <StatusBar style="auto" />
             </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    
     container: {
-        justifyContent: "center",
-        alignItems: "center",
         flex: 1,
         padding: 15,
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        marginBottom: 0,
+        alignItems: "flex-start", 
     },
     txtTitulo: {
-        fontSize: 30,
+        fontSize: 25,
         fontWeight: 'bold',
         color: 'black',
         textAlign: 'center',
         marginBottom: 10,
+        alignSelf: 'stretch'  // Se asegura que el título ocupe todo el ancho
     },
     txtSubtitulo: {
         fontSize: 15,
         color: 'black',
-        textAlign: 'right',
-        marginBottom: 20,
+        textAlign: 'left', 
+        marginBottom: 30,
     },
     inputContainer: {
         flexDirection: 'row',
@@ -95,29 +83,36 @@ const styles = StyleSheet.create({
         color: 'black',
     },
     btnLogin: {
-        borderRadius: 70,
-        width: 300,
-        height: 45,
+        width: "100%",
+        height: 38,
+        backgroundColor: "#B72424",
+        borderRadius: 10,
         justifyContent: "center",
         alignItems: "center",
-        marginBottom: 20,
+        marginTop: 10,
     },
     txtLoginButton: {
-        fontSize: 18,
-        color: '#FFFFFF',
-        fontWeight: 'bold',
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
     },
     txtLogin: {
         fontSize: 15,
         color: 'black',
-        textAlign: 'right',
+        textAlign: 'left',
     },
     txtForgot: {
         fontSize: 15,
+        fontWeight: 'bold',
         color: 'black',
-        textAlign: 'right',
+        textAlign: 'left', 
         textDecorationLine: 'underline',
-        marginBottom: 20,
+        marginBottom: 30,
+    },
+    txtNonAccount:{
+        fontSize: 15,
+        color: 'black',
+        alignItems: 'center',  // Centrado
     },
     txtSignUp: {
         color: '#FF0000',
